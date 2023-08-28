0b2adae8c619e8fd33fdbc78f428f01307fc97ec
fanisvl <fanis.vlahogiannis@gmail.com>
2023-08-28 10:33:43 +0300

    wrapped logic to main method

--- app.py
+++ app.py
@@ -3,37 +3,37 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
+def main():
 
+    op = webdriver.ChromeOptions()
+    op.add_argument('headless') 
 
-op = webdriver.ChromeOptions()
-op.add_argument('headless') 
+    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=op)
+    driver.get('https://www.villagecinemas.gr/WebTicketing/')
 
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=op)
-driver.get('https://www.villagecinemas.gr/WebTicketing/')
+    cinema = driver.find_element(By.XPATH, "//*[@id=\"cinemaSelection\"]/div/div/div[1]/a")
+    cinema.click()
 
-cinema = driver.find_element(By.XPATH, "//*[@id=\"cinemaSelection\"]/div/div/div[1]/a")
-cinema.click()
+    movie = driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[2]/div[1]/div[2]/div[5]")
+    movie.click()
 
-movie = driver.find_element(By.XPATH, "/html/body/div[2]/div[2]/div[2]/div[1]/div[2]/div[5]")
-movie.click()
+    try_dates = ["0831", "0901", "0902", "0903", "0904",
+                "0905", "0906", "0907", "0908", "0909",
+                "0910","0911","0912","0913","0914","0915",]
 
-try_dates = ["0831", "0901", "0902", "0903", "0904",
-              "0905", "0906", "0907", "0908", "0909",
-              "0910","0911","0912","0913","0914","0915",]
+    new_date_available = False
+    for try_date in try_dates:
+        find = "#date2023" + try_date
+        try:
+            datepicks = driver.find_element(By.CSS_SELECTOR, find)
+            new_date_available = True
 
-new_date_available = False
-for try_date in try_dates:
-    find = "#date2023" + try_date
-    try:
-        datepicks = driver.find_element(By.CSS_SELECTOR, find)
-        new_date_available = True
+        except:
+            pass
 
-    except:
-        pass
+    print()
 
-print()
-
-if new_date_available:
-    print("New date found!")
-else:
-    print("No new dates yet.")
+    if new_date_available:
+        print("New date found!")
+    else:
+        print("No new dates yet.")
