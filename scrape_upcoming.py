71374a392279b1553104c58ba657ae4f709f94e6
fanisvl <fanis.vlahogiannis@gmail.com>
2023-09-11 12:00:04 +0300

    remove unnecessary import

--- scrape_upcoming.py
+++ scrape_upcoming.py
@@ -2,7 +2,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from time import sleep
 
 def main():
     op = webdriver.ChromeOptions()
