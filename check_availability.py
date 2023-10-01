ccfdeb8469ad89a043c7f064c1b04cf11a83ca96
fanisvl <fanis.vlahogiannis@gmail.com>
2023-10-01 20:51:40 +0300

    implement check loop

--- check_availability.py
+++ check_availability.py
@@ -5,19 +5,21 @@ from selenium.webdriver.common.by import By
 
 from send_email import send_email
 
+from time import sleep
+
 
 
 def main():
 
+
+    # Initialize browser
     op = webdriver.ChromeOptions()
-    op.add_argument('headless') 
-
+    # op.add_argument('headless') 
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=op)
-    driver.get('https://www.villagecinemas.gr/WebTicketing/')
 
+    driver.get('https://www.villagecinemas.gr/WebTicketing/')
     cinema = driver.find_element(By.XPATH, "//*[@id=\"cinemaSelection\"]/div/div/div[1]/a")
     cinema.click()
-
     try:
         movie = driver.find_element(By.CSS_SELECTOR, "div[data-vc-movie='HO00105706']")
         movie.click()
@@ -25,29 +27,27 @@ def main():
         print("Movie not found")
         exit()
 
-    try_dates = ["0831", "0901", "0902", "0903", "0904",
-                "0905", "0906", "0907", "0908", "0909",
-                "0910","0911","0912","0913","0914","0915"]
+    while True:
+        # Should be implemented using dates library
+        try_dates = ["1005"]
+        new_date_found = False
+        for try_date in try_dates:
+            find = "#date2023" + try_date
+            try:
+                driver.find_element(By.CSS_SELECTOR, find)
+                new_date_found = True
+            except:
+                pass
 
-    new_date_found = False
-    for try_date in try_dates:
-        find = "#date2023" + try_date
-        try:
-            driver.find_element(By.CSS_SELECTOR, find)
-            new_date_found = True
-
-        except:
-            pass
-
-    driver.quit()
-
-    if new_date_found:
-        print("== New date found! ==")
-        send_email("fanis.vlahogiannis@gmail.com", "New dates found!", "New dates available")
-    else:
-        print("== No new dates yet. == ")
-
-    return new_date_found
+        if new_date_found:
+            print("== New date found! ==")
+            send_email("fanis.vlahogiannis@gmail.com", "New dates found!", "New dates available")
+            break
+        else:
+            print("== No new dates yet. == ")
+            interval = 3 # temporary
+            sleep(interval)
+            driver.refresh()
 
 
 if __name__ == "__main__":
