44b023c59669db3f1e281c1ece6323702230bbce
fanisvl <fanis.vlahogiannis@gmail.com>
2023-10-02 13:53:37 +0300

    add loop

--- check_upcoming.py
+++ check_upcoming.py
@@ -15,15 +15,25 @@ def main():
 
     driver.get('https://www.villagecinemas.gr/WebTicketing/')
 
-    #TODO:  Receive upcoming titles from scrape_upcoming
 
     titles = driver.find_elements(By.CSS_SELECTOR, "h5.media-heading")
     titles = [title.accessible_name for title in titles]
+
+    #TODO:  Receive upcoming titles from scrape_upcoming
     find_movie = input("Search for movie: ")
-    for title in titles:
-        if (title.__contains__(find_movie.upper())):
-            print(title)
-            print("Found!")
+
+    attempt = 0
+    while True:
+        attempt += 1
+        for title in titles:
+            if (title.__contains__(find_movie.upper())):
+                print(title)
+                send_email("fanis.vlahogiannis@gmail.com", "New dates found!", "New dates available")
+                print("Movie has been released!")
+            else:
+                print(" Movie has not released yet")
+                wait(10, attempt)
+                driver.refresh()
 
 if __name__ == "__main__":
     main()
