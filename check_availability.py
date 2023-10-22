ebbacf1fce44e446830e34397d8146db63b02cbf
fanisvl <fanis.vlahogiannis@gmail.com>
2023-10-22 19:31:49 +0300

    add alert logic

--- check_availability.py
+++ check_availability.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from send_email import send_email
 from time import sleep
 import os
+from database import get_alerts, delete_alert, get_title_by_id
 
 def main():
 
@@ -15,26 +16,18 @@ def main():
 
     driver.get('https://www.villagecinemas.gr/WebTicketing/')
 
-
     available_title_elements = driver.find_elements(By.CSS_SELECTOR, "h5.media-heading")
     available_titles = [title.accessible_name for title in available_title_elements]
 
-    # Replace with database logic
-    attempt = 0
-    movieFound = False
-    while True:
-        attempt += 1
-        for title in available_titles:
-            if title.__contains__(find_movie.upper()):
-                movieFound = True
-
-        if movieFound:
-            print(find_movie.capitalize() + " found")
-            send_email("fanis.vlahogiannis@gmail.com", f"Tickets available for {find_movie.capitalize()}", f"Tickets available for {find_movie.capitalize()}")
-            break
-        else:
-            wait(10, attempt)
-            driver.refresh()
+    alerts = get_alerts()
+    for alert in alerts:
+         alert_id = alert[0]
+         email = alert[1]
+         movie_id = alert[2]
+         title = get_title_by_id(movie_id)
+         if title in available_titles:
+              send_email(email, f"Tickets available for {title.capitalize()}", "Tickets now available!")
+              delete_alert(alert_id)
 
 
 def clear():
