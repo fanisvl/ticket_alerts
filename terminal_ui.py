003ad3b90cc6e8f72d377880e06660d77ccf9104
fanisvl <fanis.vlahogiannis@gmail.com>
2023-11-14 20:59:07 +0200

    pick from titles that don't have tickets already available

--- terminal_ui.py
+++ terminal_ui.py
@@ -1,13 +1,14 @@
 from pick import pick
-from database import get_upcoming_titles, post_alert
+from database import get_upcoming_titles, post_alert, has_tickets_available
 
 
 def main():
 
     upcoming_titles = get_upcoming_titles()
+    unavailable_tickets = [title for title in upcoming_titles if not has_tickets_available(title)]
     prompt = 'Select an upcoming movie to track: '
     # pick returns tuple (option_picked, index)
-    selected_title = pick(upcoming_titles, prompt)[0] 
+    selected_title = pick(unavailable_tickets, prompt)[0] 
     print(f"You selected {selected_title}.")
 
     email = input("Enter your email to be notified: ") or "fanis.vlahogiannis@gmail.com" # default
