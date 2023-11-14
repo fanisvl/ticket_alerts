175636809b71937c4b1a0b01dce9c8b92fda1bfb
fanisvl <fanis.vlahogiannis@gmail.com>
2023-11-14 12:00:28 +0200

    merge check_alerts into check_all

--- check_all.py
+++ check_all.py
@@ -3,7 +3,8 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import os
-from database import get_upcoming_movies, set_tickets_available_true
+from database import get_upcoming_movies, set_tickets_available_true, has_tickets_available, get_movie_data, get_alerts, delete_alert
+from send_email import send_email
 
 def main():
 
@@ -15,22 +16,33 @@ def main():
     If so, update ticketsAvailable attribute to true.
     """
 
-    # Initialize browser
-    op = webdriver.ChromeOptions()
-    op.add_argument('headless') 
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=op)
-    driver.get('https://www.villagecinemas.gr/WebTicketing/')
+    # # Initialize browser
+    # op = webdriver.ChromeOptions()
+    # op.add_argument('headless') 
+    # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=op)
+    # driver.get('https://www.villagecinemas.gr/WebTicketing/')
     
-    # The h5.media-heading selector includes titles in both english & greek
-    available_title_elements = driver.find_elements(By.CSS_SELECTOR, "h5.media-heading")
-    available_titles = [title.accessible_name for title in available_title_elements]
+    # # The h5.media-heading selector includes titles in both english & greek
+    # available_title_elements = driver.find_elements(By.CSS_SELECTOR, "h5.media-heading")
+    # available_titles = [title.accessible_name for title in available_title_elements]
 
-    # *_ is used to ignore the rest of the values inside of each tuple, we only need id, title
-    for id, title, *_ in get_upcoming_movies():
-        if title in available_titles:
-            set_tickets_available_true(id)
-    driver.close()
+    # # update ticket availability of all upcoming movies
+    # # *_ is used to ignore the rest of the values inside of each tuple, we only need id, title
+    # for id, title, *_ in get_upcoming_movies():
+    #     if title in available_titles:
+    #         set_tickets_available_true(id)
+    # driver.close()
 
+    alerts = get_alerts() # returns a list of tuples: (alert_id, email, movie_title)
+    print(alerts)
+
+    for alert in alerts:
+        if has_tickets_available(alert["movie_title"]):
+            movie_data = get_movie_data(alert["movie_title"])
+            send_email(alert["email"], movie_data)
+            set_tickets_available_true(alert["movie_title"])
+            delete_alert(alert["alert_id"])
+            
 def clear():
     """Clear terminal scren"""
     if os.name == 'posix':
