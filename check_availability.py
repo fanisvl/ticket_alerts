3a085741624884ae46bf131b21ec9d6e19690507
fanisvl <fanis.vlahogiannis@gmail.com>
2023-11-20 19:27:56 +0200

    pep8, format

--- check_availability.py
+++ check_availability.py
@@ -3,11 +3,12 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 import os
-from database import get_upcoming_movies, set_tickets_available_true, has_tickets_available, get_movie_data, get_alerts, delete_alert
+from database import get_upcoming_movies, set_tickets_available_true, has_tickets_available,\
+    get_movie_data, get_alerts, delete_alert
 from send_email import send_email
 
-def main():
 
+def main():
     """
     Checks ticket availability of all Upcoming Movies & updates ticketAvailability attributes on database. 
     If an upcoming movie has tickets available and alerts set up, the alerts are sent and then deleted.
@@ -15,10 +16,10 @@ def main():
 
     # Initialize browser
     op = webdriver.ChromeOptions()
-    op.add_argument('headless') 
+    op.add_argument('headless')
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=op)
     driver.get('https://www.villagecinemas.gr/WebTicketing/')
-    
+
     # The h5.media-heading selector includes titles in both english & greek
     available_title_elements = driver.find_elements(By.CSS_SELECTOR, "h5.media-heading")
     available_titles = [title.accessible_name for title in available_title_elements]
@@ -30,19 +31,21 @@ def main():
     driver.close()
 
     # Send alerts if necessary
-    alerts = get_alerts() # returns a list of tuples: (alert_id, email, movie_title)
+    alerts = get_alerts()  # returns a list of tuples: (alert_id, email, movie_title)
     for alert in alerts:
         if has_tickets_available(alert["movie_title"]):
             movie_data = get_movie_data(alert["movie_title"])
             send_email(alert["email"], movie_data)
             delete_alert(alert["alert_id"])
-            
+
+
 def clear():
-    """Clear terminal scren"""
+    """Clear terminal screen"""
     if os.name == 'posix':
         os.system('clear')
     else:
         os.system('cls')
+
 
 if __name__ == "__main__":
     main()
