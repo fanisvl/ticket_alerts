a11d15bf8abedc6ef21f405188e0e2589c273354
fanisvl <fanis.vlahogiannis@gmail.com>
2023-12-02 14:38:20 +0200

    seperate update availability and send alerts functionality

--- check_availability.py
+++ check_availability.py
@@ -2,16 +2,19 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
-from database import get_upcoming_movies, set_tickets_available_true, has_tickets_available,\
+from database import get_upcoming_movies, set_tickets_available_true, has_tickets_available, \
     get_movie_data, get_alerts, delete_alert
 from send_email import send_email
 
 
 def main():
-    """
-    Checks ticket availability of all Upcoming Movies & updates ticketAvailability attributes on database. 
-    If an upcoming movie has tickets available and alerts set up, the alerts are sent and then deleted.
-    """
+    """"update_availability and send_alerts are in the same file since they are executed consecutively"""
+    update_availability()
+    send_alerts()
+
+
+def update_availability():
+    """Scrapes the cinema website to update ticket availability in the database."""
 
     # Initialize browser
     op = webdriver.ChromeOptions()
@@ -22,14 +25,17 @@ def main():
     # The h5.media-heading selector includes titles in both english & greek
     available_title_elements = driver.find_elements(By.CSS_SELECTOR, "h5.media-heading")
     available_titles = [title.accessible_name for title in available_title_elements]
+    driver.close()
 
-    # Update ticketAvailability of all upcoming movies
+    # Update database ticketAvailability of all upcoming movies
     for movie in get_upcoming_movies():
         if movie["title"] in available_titles:
             set_tickets_available_true(movie["title"])
-    driver.close()
 
-    # Send alerts if necessary
+
+def send_alerts():
+    """Sends alerts for available tickets and deletes them from the database."""
+
     alerts = get_alerts()  # returns a list of tuples: (alert_id, email, movie_title)
     for alert in alerts:
         if has_tickets_available(alert["movie_title"]):
